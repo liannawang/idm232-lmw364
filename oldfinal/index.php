7fd9bbb1a62054793abc1760b79d0ad0f59342be
liannawang <91167762+liannawang@users.noreply.github.com>
2022-11-17 00:05:01 -0500

    😁 assignment 2 first draft

---
+++ oldfinal/index.php
@@ -0,0 +1,72 @@
+<?php include_once 'header.php'; ?>
+<div class = "explore">
+<h1> Explore our categories:</h1>
+<div class = "category-row">
+<img src= "pictures/Appetizer.png">
+<img src= "pictures/Breakfast.png">
+<img src= "pictures/Lunch.png">
+<img src= "pictures/Dinner.png">
+<img src= "pictures/Dessert.png">
+</div>
+</div>
+
+
+<h2 class = "heading">Popular Recipes</h2>
+<div class = "cards">
+<div class="card-container">
+  <div class="section">
+  <div class = "card">
+    <img src = "https://via.placeholder.com/350x250">
+    <h3 class = "rating"> ⭐️⭐️⭐️⭐️⭐️</h3>
+    <h3 class = "title">Recipe Name</h3>
+</div>
+  </div>
+  <div class="section">
+  <div class = "card">
+    <img src = "https://via.placeholder.com/350x250">
+    <h3 class = "rating"> ⭐️⭐️⭐️⭐️⭐️</h3>
+    <h3 class = "title">Recipe Name</h3>
+</div>
+  </div>
+  <div class="section">
+      <div class = "card">
+    <img src = "https://via.placeholder.com/350x250">
+    <h3 class = "rating"> ⭐️⭐️⭐️⭐️⭐️</h3>
+    <h3 class = "title">Recipe Name</h3>
+</div>
+  </div>
+</div>
+
+</div>
+<h2 class = "heading">Category Heading</h2>
+<div class = "cards">
+<div class="card-container">
+  <div class="section">
+  <div class = "card">
+    <img src = "https://via.placeholder.com/350x250">
+    <h3 class = "rating"> ⭐️⭐️⭐️⭐️⭐️</h3>
+    <h3 class = "title">Recipe Name</h3>
+</div>
+  </div>
+  <div class="section">
+  <div class = "card">
+    <img src = "https://via.placeholder.com/350x250">
+    <h3 class = "rating"> ⭐️⭐️⭐️⭐️⭐️</h3>
+    <h3 class = "title">Recipe Name</h3>
+</div>
+  </div>
+  <div class="section">
+      <div class = "card">
+    <img src = "https://via.placeholder.com/350x250">
+    <h3 class = "rating"> ⭐️⭐️⭐️⭐️⭐️</h3>
+    <h3 class = "title">Recipe Name</h3>
+</div>
+  </div>
+</div>
+
+</div>
+<?php include_once 'footer.php'; ?>
+
+
+
+
